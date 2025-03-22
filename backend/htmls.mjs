cf61875ee6367a87d6f64949053e05d952bd532c
Nick-Lemy <n.kayiranga@alustudent.com>
2025-03-23 00:17:18 +0200

    update layout

--- backend/htmls.mjs
+++ backend/htmls.mjs
@@ -59,7 +59,7 @@ export const returnEvent = (
             </div>
           </li>
           <li>
-            <div class="flexr">
+            <div class="flex">
               <svg class="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
               </svg>
@@ -138,7 +138,7 @@ export const returnEvent = (
   });
   const registerButton = document.getElementById('register-btn')
   registerButton.addEventListener('click', ()=>{
-    
+
     })
     </script>
   </body>
